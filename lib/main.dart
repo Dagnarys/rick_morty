62cf07bbce244eef0622eaa5b9ef40165afd870c
dagnarys <super.iard@yandex.ru>
2025-08-25 23:04:01 +0300

    added multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_morty/bloc/cubit/character_cubit.dart';
+import 'package:rick_morty/data/services/character_service.dart';
 import 'package:rick_morty/screens/main_screen.dart';
 
 void main() {
@@ -9,9 +12,20 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (_) =>
+                  CharacterCubit(characterService: CharacterService())
+                    ..loadCharacters(),
+        ),
+      ],
+
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MainScreen()
+      ),
     );
   }
 }
