909cf55ccaacce582c658599d820fd20d260033f
dagnarys <super.iard@yandex.ru>
2025-08-25 23:04:19 +0300

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_morty/bloc/cubit/character_cubit.dart';
+import 'package:rick_morty/bloc/state/character_state.dart';
+
 import 'package:rick_morty/widgets/bottom_navigation.dart';
 import 'package:rick_morty/widgets/character_card.dart';
 
@@ -9,23 +13,33 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Rick and Morty'), centerTitle: true),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        scrollDirection: Axis.vertical,
-        itemCount: 8,
-        itemBuilder: (context, index) {
-          return Container(
-            padding: const EdgeInsets.all(0),
-            child: CharacterCard(),
-          );
-        },
-      ),
-
       bottomNavigationBar: BottomAppBar(
         padding: EdgeInsets.all(0),
         child: BottomNavigation(),
+      ),
+      body: BlocBuilder<CharacterCubit, CharacterState>(
+        builder: (context, state) {
+          if (state is CharacterLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state is CharacterLoaded) {
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              scrollDirection: Axis.vertical,
+              itemCount: state.characters.length,
+              itemBuilder: (context, index) {
+                final character = state.characters[index];
+                return Container(
+                  padding: const EdgeInsets.all(0),
+                  child: CharacterCard(character: character),
+                );
+              },
+            );
+          }
+          return Center(child: Text('Данные не найдены'));
+        },
       ),
     );
   }
