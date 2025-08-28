16933580c424731091e5d4541bd4e3c224a2b9e3
dagnarys <super.iard@yandex.ru>
2025-08-28 21:36:25 +0300

    added theme cubit

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_morty/bloc/cubit/theme_cubit.dart';
+import 'package:rick_morty/bloc/state/theme_state.dart';
 import 'package:rick_morty/screens/favorite_screen.dart';
 import 'package:rick_morty/screens/main_screen.dart';
 
@@ -7,35 +10,47 @@ class BottomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(top: BorderSide(color: Colors.black, width: 2)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(Icons.home, size: 44),
-            onPressed: () {
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => MainScreen()),
-                (route) => false,
-              );
-            },
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, themeState) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.black, width: 2)),
           ),
-          IconButton(
-            icon: Icon(Icons.favorite, size: 44),
-            onPressed: () {
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => FavoriteScreen()),
-                (route) => false,
-              );
-            },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                color:
+                    themeState.isDarkMode
+                        ? const Color.fromARGB(255, 121, 118, 118)
+                        : Colors.black,
+                icon: Icon(Icons.home, size: 44),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainScreen()),
+                    (route) => false,
+                  );
+                },
+              ),
+              IconButton(
+                color:
+                    themeState.isDarkMode
+                        ? const Color.fromARGB(255, 121, 118, 118)
+                        : Colors.black,
+                icon: Icon(Icons.star, size: 44),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoriteScreen()),
+                    (route) => false,
+                  );
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
