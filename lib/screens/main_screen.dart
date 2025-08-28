a94b7c0327f47fc024ec085cac86342e3197d763
dagnarys <super.iard@yandex.ru>
2025-08-28 21:36:17 +0300

    added theme cubit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_morty/bloc/cubit/character_cubit.dart';
+import 'package:rick_morty/bloc/cubit/theme_cubit.dart';
 import 'package:rick_morty/bloc/state/character_state.dart';
+import 'package:rick_morty/bloc/state/theme_state.dart';
 
 import 'package:rick_morty/widgets/bottom_navigation.dart';
 import 'package:rick_morty/widgets/character_card.dart';
@@ -11,57 +13,82 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/img/bg_img.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text(
-            'Rick and Morty',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(state.currentImage),
+              fit: BoxFit.cover,
+            ),
           ),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-        ),
-        bottomNavigationBar: BottomAppBar(
-          padding: EdgeInsets.all(0),
-          child: BottomNavigation(),
-        ),
-        body: BlocBuilder<CharacterCubit, CharacterState>(
-          builder: (context, state) {
-            if (state is CharacterLoading) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (state is CharacterLoaded) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2),
-                child: GridView.builder(
-                  padding: EdgeInsets.all(16),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 1,
-                    crossAxisSpacing: 12,
-                    childAspectRatio: 0.8,
-                  ),
-
-                  scrollDirection: Axis.vertical,
-                  itemCount: state.characters.length,
-                  itemBuilder: (context, index) {
-                    final character = state.characters[index];
-                    return CharacterCard(character: character);
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    context.read<ThemeCubit>().toggleTheme();
                   },
+                  color: state.isDarkMode ? Colors.white : Colors.black,
+                  icon: Icon(state.isDarkMode ?Icons.light_mode:Icons.dark_mode, size: 36),
                 ),
-              );
-            }
-            return Center(child: Text('Данные не найдены'));
-          },
-        ),
-      ),
+              ],
+              title: Text(
+                'Rick and Morty',
+                style: TextStyle(
+                  color:
+                      context.read<ThemeCubit>().state.isDarkMode
+                          ? Colors.white
+                          : Colors.black,
+
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              centerTitle: true,
+              backgroundColor: Colors.transparent,
+            ),
+            bottomNavigationBar: BottomAppBar(
+              color:
+                  state.isDarkMode
+                      ? Colors.transparent
+                      : const Color.fromARGB(255, 255, 255, 255),
+              padding: EdgeInsets.all(0),
+              child: BottomNavigation(),
+            ),
+            body: BlocBuilder<CharacterCubit, CharacterState>(
+              builder: (context, state) {
+                if (state is CharacterLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (state is CharacterLoaded) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: GridView.builder(
+                      padding: EdgeInsets.all(16),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 1,
+                        crossAxisSpacing: 12,
+                        childAspectRatio: 0.8,
+                      ),
+
+                      scrollDirection: Axis.vertical,
+                      itemCount: state.characters.length,
+                      itemBuilder: (context, index) {
+                        final character = state.characters[index];
+                        return CharacterCard(character: character);
+                      },
+                    ),
+                  );
+                }
+                return Center(child: Text('Данные не найдены'));
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
