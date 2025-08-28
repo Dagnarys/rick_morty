87783c83c9a34667882c4cb7144eb19f7e81cbcc
dagnarys <super.iard@yandex.ru>
2025-08-28 21:34:01 +0300

    added sort and hive

--- lib/bloc/cubit/character_cubit.dart
+++ lib/bloc/cubit/character_cubit.dart
@@ -3,6 +3,7 @@ import 'package:rick_morty/bloc/state/character_state.dart';
 import 'package:rick_morty/data/models/character_model.dart';
 
 import 'package:rick_morty/data/services/character_service.dart';
+import 'package:rick_morty/data/services/hive_service.dart';
 
 class CharacterCubit extends Cubit<CharacterState> {
   final CharacterService characterService;
@@ -19,9 +20,28 @@ class CharacterCubit extends Cubit<CharacterState> {
     }
   }
 
-  void toggleFavorite(int characterId) {
+  Future<void> refreshCharacters() async {
+    try {
+      final characters = await characterService.getCharacters(
+        forceRefresh: true,
+      );
+      emit(CharacterLoaded(characters));
+    } catch (e) {
+      emit(CharacterError('Failed to refresh characters: $e'));
+    }
+  }
+
+  void toggleFavorite(int characterId) async {
     if (state is CharacterLoaded) {
       final currentState = state as CharacterLoaded;
+      final character = currentState.characters.firstWhere(
+        (character) => character.id == characterId,
+      );
+      if (character.favorite) {
+        await HiveService.removeFromFavorites(characterId);
+      } else {
+        await HiveService.addToFavorites(characterId);
+      }
       final updatedCharacters =
           currentState.characters.map((character) {
             if (character.id == characterId) {
@@ -41,9 +61,35 @@ class CharacterCubit extends Cubit<CharacterState> {
   }
 
   List<Character> get favoriteCharacters {
-    return (state as CharacterLoaded).characters
-        .where((character) => character.favorite)
-        .toList();
-    
+    if (state is CharacterLoaded) {
+      final currentState = state as CharacterLoaded;
+      final favorites =
+          currentState.characters.where((c) => c.favorite).toList();
+
+      return _sortFavorites(favorites);
+    }
+    return [];
+  }
+
+  String _favoritesSortBy = 'name';
+
+  String get favoritesSortBy => _favoritesSortBy;
+
+  void changeFavoritesSorting(String sortBy) {
+    _favoritesSortBy = sortBy;
+     if (state is CharacterLoaded) {
+      final currentState = state as CharacterLoaded;
+      emit(CharacterLoaded(currentState.characters)); 
+    }
+  }
+
+  List<Character> _sortFavorites(List<Character> favorites) {
+    final sorted = List<Character>.from(favorites);
+    if (_favoritesSortBy == 'name') {
+      sorted.sort((a, b) => a.name.compareTo(b.name));
+    } else {
+      sorted.sort((a, b) => a.id.compareTo(b.id));
+    }
+    return sorted;
   }
 }
